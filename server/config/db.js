c57542b355b8eb93914143f86de9881e191eb0f2
John Doe <jkim458@my.centennialcollge.ca>
2020-10-27 16:02:24 -0400

    comment for student's info

--- server/config/db.js
+++ server/config/db.js
@@ -4,3 +4,9 @@ module.exports = {
 
   "URI" : "mongodb+srv://kjd199227:kjd15209@cluster0.t9jin.mongodb.net/books229?retryWrites=true&w=majority"
 };
+
+// File Name    : COMP229-F2020-MidTerm-301105279
+// Author Name  : JiHyeok Kim
+// StudentID    : 301105279
+// Web App name : comp229-f2020-midterm301105279
+    
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,3 +1,9 @@
+// File Name    : COMP229-F2020-MidTerm-301105279
+// Author Name  : JiHyeok Kim
+// StudentID    : 301105279
+// Web App name : comp229-f2020-midterm301105279
+
+
 // modules required for routing
 let express = require('express');
 let router = express.Router();
