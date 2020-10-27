17c1f5eca3ca0342457740cec62e8f9eb67ef070
John Doe <jkim458@my.centennialcollge.ca>
2020-10-27 15:36:27 -0400

    Create new database on MongoDBAtlas and change URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  "URI" : "mongodb+srv://kjd199227:kjd15209@cluster0.t9jin.mongodb.net/books229?retryWrites=true&w=majority"
 };
